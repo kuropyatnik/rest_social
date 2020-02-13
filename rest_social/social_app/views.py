59ceef3e48f23c5f8556f574a61210cb3119cbdb
Danylo Kuropiatnyk <dkuropyatnik27@activewizards.com>
2020-02-13 03:11:08 +0200

    Create the endpoint for post adding

--- rest_social/social_app/views.py
+++ rest_social/social_app/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.exceptions import ParseError
 from django.contrib.auth.hashers import make_password
 from rest_framework.views import status
 from rest_social.settings import SECRET_KEY
-from .serializers import UserSerializer, LoginSerializer
+from .serializers import UserSerializer, LoginSerializer, PostSerializer
 from .models import User, Post
+from .authentication import TokenAuthentication
 import jwt
 
 
@@ -56,5 +57,33 @@ def login_view(request):
         status_code = status.HTTP_400_BAD_REQUEST
     except ParseError:
         data = {'Error': 'Bad request'}
+        status_code = status.HTTP_400_BAD_REQUEST
+    return Response(data=data, status=status_code)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated, ])
+@authentication_classes([TokenAuthentication, ])
+def add_post_view(request):
+    authentication_classes(TokenAuthentication, )
+    user_data = jwt.decode(bytes(request.headers.get('token'), 'utf-8'), SECRET_KEY, ['HS256'])
+    user = User.objects.get(username=user_data['username'])
+    try:
+        serialized_post = PostSerializer(data=request.data, context={'request': request})
+        if serialized_post.is_valid():
+            title = str(serialized_post.initial_data['title'])
+            content = str(serialized_post.initial_data['content'])
+            if title.strip() == '' or content.strip() == '':
+                data = {'Error': 'At least one required field is empty'}
+                status_code = status.HTTP_400_BAD_REQUEST
+            else:
+                Post.objects.create(creator=user, title=title, content=content)
+                data = {"Result": "Post was added"}
+                status_code = status.HTTP_200_OK
+        else:
+            data = {key: str(value[0]) for key, value in serialized_post.errors.items()}
+            status_code = status.HTTP_400_BAD_REQUEST
+    except Exception as e:
+        data = {'Error': 'Bad request ' + str(e)}
         status_code = status.HTTP_400_BAD_REQUEST
     return Response(data=data, status=status_code)
